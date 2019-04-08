01c5a524f3b4143379060d0c7421d3767016a485
sonet-sunet <slava.zhukov@gmail.com>
2019-04-08 10:07:53 +0300

    initial commit

---
+++ parts/footer.php
@@ -0,0 +1,7 @@
+    </div>
+    <?php foreach( $pageConfig['jsFiles'] as $path_js): ?>
+        <script src="<?=$path_js?>"></script>
+    <?php endforeach; ?>
+    <div class='footer'>Это наш футер</div>
+</body>
+</html> 
