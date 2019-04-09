584a8fe3ab4347ff930d9cc56dec1ac8ae09a60a
StVictoria <2018vvs@gmail.com>
2019-04-09 17:44:15 +0300

    отред БД в config

--- config/configuration.php
+++ config/configuration.php
@@ -1,3 +1,3 @@
 <?php
-    $db = mysqli_connect('localhost', 'root', '', '05120218_3project');
+    $db = mysqli_connect('localhost', 'root', '', '05122018_3project');
     mysqli_set_charset($db, 'utf8');
