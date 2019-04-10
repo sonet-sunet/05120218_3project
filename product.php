096fe38c9b27d610b5cbfb6f87917cff03722fc4
StVictoria <2018vvs@gmail.com>
2019-04-11 00:59:39 +0300

    Отредактированы стили карточки продукта

--- product.php
+++ product.php
@@ -22,7 +22,7 @@ if(isset($_GET['id'])) {
     $result_products = mysqli_query($db, $sql_products);
 
     $template['product'] = mysqli_fetch_assoc($result_products);
-    d($template);  
+    // d($template);  
     
     $sql_product_catalog = "SELECT * FROM product_catalog WHERE product_id = {$_GET['id']}";
     $result_product_catalog = mysqli_query($db, $sql_product_catalog);
@@ -33,7 +33,7 @@ if(isset($_GET['id'])) {
     $result_catalog = mysqli_query($db, $sql_catalog);
 
     $template['catalog'] = mysqli_fetch_assoc($result_catalog);
-    d($template);  
+    // d($template);  
 
 } else {
     header("HTTP/1.1 303 See Other");
@@ -43,12 +43,16 @@ if(isset($_GET['id'])) {
 
 <?php if(!empty($template['product'])): ?>
     <div class="product">
-        <img src="<?=$template['product']['img_src']?>">
-        <h3><?=$template['product']['name']?></h3>
-        <p><?=$template['product']['price']?> рублей</p>
-        <p><?=$template['product']['description']?></p>
-        <p><?=$template['catalog']['name']?></p>
-        <button data-product-id = '<?=$template['product']['id']?>' class='add-to-basket'>Добавить в корзину</button>
+        <div class="product_photo">
+            <div style="background: url('<?=$template['product']['img_src']?>') center center / contain no-repeat;"></div>
+        </div>
+        <div class="product_main_info">
+            <h3><?=$template['product']['name']?></h3>
+            <div class="sku">Артикул: <?=$template['product']['sku']?></div>
+            <div class="price" style="margin: 15px 0;"><i><?=$template['product']['price']?> руб.</i></div>
+            <p><?=$template['product']['description']?></p>
+            <button data-product-id = '<?=$template['product']['id']?>' class='add-to-basket'>Добавить в корзину</button>
+        </div>
     </div>
 <?php else: ?>
     <h2>Такого товара нет</h2>
